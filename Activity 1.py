5695f280675c80c9ec154fa6cd7aee3201d5cc34
tejasvmittal12-bit <tejasvmittal12@gmail.com>
2026-06-23 20:55:43 +0530

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,10 @@
+print("hello My Name Is Tejasv💀")
+print("I Am 11 Years Old😁")
+print("I Live In India")
+print(10)
+print("\nHello world🌍")
+import keyword
+print (keyword.kwlist)
+print(3+5)
+x = 3+5
+print(x)
