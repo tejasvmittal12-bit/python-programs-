1761bbc528010c9154fdad3fb06e0b9fe0c56ef3
tejasvmittal12-bit <tejasvmittal12@gmail.com>
2026-07-12 14:11:01 +0530

    Add files via upload

---
+++ fibbonacci.py
@@ -0,0 +1,55 @@
+print("Fibonacci Sequence:")
+a = 0
+print(a)
+b = 1
+print(b)
+c = a + b
+print(c)
+d = b + c
+print(d)
+e = c + d
+print(e)
+f = d + e
+print(f)
+g = e + f
+print(g)
+h = f + g
+print(h)
+i = g + h
+print(i)
+j = h + i
+print(j)
+k = i + j
+print(k)
+l = j + k
+print(l)
+m = k + l
+print(m)
+n = l + m
+print(n)
+o = m + n
+print(o)
+p = n + o
+print(p)
+q = o + p
+print(q)
+r = p + q
+print(r)
+s = q + r
+print(s)
+t = r + s
+print(t)
+u = s + t
+print(u)
+v = t + u
+print(v)
+w = u + v
+print(w)
+x = v + w
+print(x)
+y = w + x
+print(y)
+z = x + y
+print(z)
+aa = y + z
+print(aa)
