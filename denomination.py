58c70e29fdf70bf527f248a4a859b531cb85dda1
tejasvmittal12-bit <tejasvmittal12@gmail.com>
2026-08-20 21:15:03 +0530

    Add files via upload

--- denomination.py
+++ denomination.py
@@ -1,69 +1,85 @@
-from tkinter import*
+from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
 
+# Setting up Main Window
 root = Tk()
-root.title('Denomination counter')
+root.title('Denomination Counter')
 root.configure(bg='light blue')
 root.geometry('650x400')
 
+# Adding Image and Labels in the Main Window
 upload = Image.open("koe.jpg")
-upload = upload.resize((300,300))
+# Resize the image using resize() method
+upload = upload.resize((300, 300))
 image = ImageTk.PhotoImage(upload)
 label = Label(root, image=image, bg='light blue')
 label.place(x=180, y=20)
-label1 = Label(root, text="Hey User! Welcome to Denomination counter Application", bg='light blue')
+
+label1 = Label(root,
+    text="Hey User! Welcome to Denomination Counter Application.",
+    bg='light blue')
 label1.place(relx=0.5, y=340, anchor=CENTER)
 
+# Function to display a messagebox and proceed if OK is clicked
 def msg():
-    MsgBox = messagebox.showinfo("Alert", "Do you want to calculate the denomination count?")
+    MsgBox = messagebox.showinfo(
+        "Alert", "Do you want to calculate the denomination count?")
     if MsgBox == 'ok':
         topwin()
 
-button1 = Button(root, text="Lets Get Started!", command=msg, bg='brown', fg='white')
-button1.place(x=260 , y=360)
+# Adding Buttons to the main window
+button1 = Button(root,
+                 text="Let's get started!",
+                 command=msg,
+                 bg='brown',
+                 fg='white')
+button1.place(x=260, y=360)
+
+# Function for opening new/top window
 def topwin():
     top = Toplevel()
-    top.title("Denomination calculator")
-    top.configure(bg="light grey")
-    top.geometry("600x350+50+50")
+    top.title("Denominations Calculator")
+    top.configure(bg='light grey')
+    top.geometry('600x350+50+50')
 
-    label= Label(top, text="Enter Total Amount", bg= 'lightgrey')
-    Entry = Entry(top)
-    lbl = Label(top, text= "Here are numbers of notes for each denomination", bg = 'lightgrey')
+    label = Label(top, text="Enter total amount", bg='light grey')
+    entry = Entry(top)
+    lbl = Label(top, text="Here are number of notes for each denomination", bg='light grey')
 
-    l1 = Label(top, text="2000", bg='lightgrey')
-    l2 = Label(top, text="500", bg='lightgrey')
-    l3 = Label(top, text="100", bg='lightgrey')
+    l1 = Label(top, text="2000", bg='light grey')
+    l2 = Label(top, text="500", bg='light grey')
+    l3 = Label(top, text="100", bg='light grey')
 
     t1 = Entry(top)
     t2 = Entry(top)
     t3 = Entry(top)
 
-    def calculator(): 
+    def calculator():
         try:
             global amount
-            amount = int(Entry.get())
-            note_2000 = amount // 2000
+            amount = int(entry.get())
+            note2000 = amount // 2000
             amount %= 2000
-            note_500 = amount // 500
+            note500 = amount // 500
             amount %= 500
-            note_100 = amount // 100
+            note100 = amount // 100
 
             t1.delete(0, END)
             t2.delete(0, END)
             t3.delete(0, END)
 
-            t1.insert(END, str(note_2000))
-            t2.insert(END, str(note_500))
-            t3.insert(END, str(note_100))
+            t1.insert(END, str(note2000))
+            t2.insert(END, str(note500))
+            t3.insert(END, str(note100))
         except ValueError:
-            messagebox.showerror("Invalid input", "Please enter a valid number.")
+            messagebox.showerror("Error", "Please enter a valid number.")
 
-    btn = Button(top, text="Calculate", command=calculator, bg='brown', fg='white')
+    btn = Button(top, text='Calculate', command=calculator, bg='brown', fg='white')
 
+    # Centering Widgets in the Top Window
     label.place(x=230, y=50)
-    Entry.place(x=200, y=80)
+    entry.place(x=200, y=80)
     btn.place(x=240, y=120)
     lbl.place(x=140, y=170)
 
